04ac9394a476cd85192410f8f5107e0fe29262a5
unknown <amirsil2002@gmail.com>
2020-10-12 13:01:24 +0300

    Project finished

--- src/Main.java
+++ src/Main.java
@@ -10,40 +10,69 @@ public class Main {
         int x2, y2; // PlayerB location
         int xRugTopLeftCorner, yRugTopLeftCorner; // Location of the rug's top left corner
         int rugLength; // Size of the rug
+        Player p;
+        Player p1 = new Player(board, -1, -1, '1');;
+        Player p2 = new Player(board, -1, -1, '2');
+        int direction;
+        boolean isRoundOver;
+        boolean isPlayerInsideCarpet;
+        boolean isGameOver = false;
+        while (!isGameOver) {
+            System.out.println("A new game is starting!");
+            x1 = safeInput(stdin, "Player1 x location: ", size);
+            y1 = safeInput(stdin, "Player1 y location:", size);
+            x2 = safeInput(stdin, "Player2 x location:", size);
+            y2 = safeInput(stdin, "Player2 y location:", size);
+            xRugTopLeftCorner = safeInput(stdin, "Rug top left corner x location:", size);
+            yRugTopLeftCorner = safeInput(stdin, "Rug top left corner y location:", size);
+            rugLength = safeRugInput(stdin, "Rug size: ", size, xRugTopLeftCorner, yRugTopLeftCorner);
 
-        x1 = safeInput(stdin, "Player1 x location: ", size);
-        y1 = safeInput(stdin, "Player1 y location:", size);
-        x2 = safeInput(stdin, "Player2 x location:", size);
-        y2 = safeInput(stdin, "Player2 y location:", size);
-        xRugTopLeftCorner = safeInput(stdin, "Rug top left corner x location:", size);
-        yRugTopLeftCorner = safeInput(stdin, "Rug top left corner y location:", size);
-        rugLength = safeRugInput(stdin, "Rug size: ", size, xRugTopLeftCorner, yRugTopLeftCorner);
-
-
-        initializeBoard(board, xRugTopLeftCorner, yRugTopLeftCorner, rugLength, x1, x2, y1, y2);
-
-        Player p1 = new Player(board, x1, y1);
-        Player p2 = new Player(board, x2, y2);
-        Player p = p1;
-        System.out.println("Player 1 is first!");
-        Thread.sleep(1000);
-        int input;
-        while (true){
-            showBoard(board);
-            try {
-                input = Integer.parseInt(stdin.nextLine());
-                if (input < 0 || input > 4) {
-                    continue;
-                }
-            } catch (Exception ignore){
+            p1.setLocation(x1, y1);
+            p2.setLocation(x2, y2);
+            isPlayerInsideCarpet = initializeBoard(board, xRugTopLeftCorner, yRugTopLeftCorner, rugLength, p1, p2);
+            if (isPlayerInsideCarpet){
+                System.out.println("Players can't start inside of the carpet! restarting game...");
                 continue;
             }
-            p.walk(input);
-            if (p == p1){
-                p = p2;
+            p = p1;
+            System.out.println("Player 1 is first!");
+            Thread.sleep(1000);
+
+            while (true) {
+                showBoard(board);
+                try {
+                    direction = Integer.parseInt(stdin.nextLine());
+                    System.out.println(direction);
+                    if (direction < 0 || direction > 4) {
+                        throw new Exception();
+                    }
+                } catch (Exception ignore) {
+                    System.out.println("Illegal move, Your turn is over");
+                    // Switch turns
+                    if (p == p1) {
+                        p = p2;
+                    } else {
+                        p = p1;
+                    }
+                    continue;
+                }
+                isRoundOver = p.walk(direction);
+                if (isRoundOver) {
+                    break;
+                }
+                // Switch turns
+                if (p == p1) {
+                    p = p2;
+                } else {
+                    p = p1;
+                }
             }
-            else {
-                p = p1;
+            System.out.format("%c is the winner!\n Player%c wins: %d\n Player%c wins: %d\n",
+                    p.getPlayerNum(), p1.getPlayerNum(), p1.getNumberOfWins(),
+                    p2.getPlayerNum(), p2.getNumberOfWins());
+
+            if (!p1.wantsToPlayAgain(stdin) || !p2.wantsToPlayAgain(stdin)){
+                isGameOver = true;
             }
         }
 
@@ -86,8 +115,7 @@ public class Main {
         }
     }
 
-
-    static void initializeBoard(char[][] board, int xRugTopLeftCorner, int yRugTopLeftCorner, int rugLength, int x1, int x2, int y1, int y2){
+    static boolean initializeBoard(char[][] board, int xRugTopLeftCorner, int yRugTopLeftCorner, int rugLength, Player p1, Player p2){
         // Initialize board with blank squares
         for (int y = 0; y < board.length; y++){
             for (int x = 0; x < board[y].length; x++){
@@ -100,8 +128,12 @@ public class Main {
                 board[x][y] = '*';
             }
         }
-        board[x1][y1] = '1';
-        board[x2][y2] = '2';
+        if (board[p1.getX()][p1.getY()] == '*' || board[p2.getX()][p2.getY()] == '*'){
+            return true;
+        }
+        board[p1.getX()][p1.getY()] = p1.getPlayerNum();
+        board[p2.getX()][p2.getY()] = p2.getPlayerNum();
+        return false;
     }
     static void showBoard(char[][] board){
         for (int y = 0; y < board.length; y++) {
@@ -112,4 +144,6 @@ public class Main {
             System.out.println();
         }
     }
+
 }
+
--- src/Player.java
+++ src/Player.java
@@ -1,18 +1,39 @@
+import java.util.Scanner;
+
 public class Player {
     char[][] board;
     private int x;
     private int y;
+    private final char playerNum;
+    private int numberOfWins;
 
-    public Player(char[][] board, int x, int y) {
+    public char getPlayerNum() {
+        return playerNum;
+    }
+
+    public int getNumberOfWins() {
+        return numberOfWins;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Player(char[][] board, int x, int y, char playerNum) {
         this.board = board;
         this.x = x;
         this.y = y;
+        this.playerNum = playerNum;
+        this.numberOfWins = 0;
     }
-     public void walk(int input) {
-        char playerNum = board[x][y];
+     public boolean walk(int direction) {
         int oldX = x, oldY = y;
         board[x][y] = '.';
-        switch (input) {
+        switch (direction) {
             case 1 -> y--;
             case 2 -> y++;
             case 3 -> x++;
@@ -20,21 +41,46 @@ public class Player {
         }
         try{
             if (board[x][y] == '*') {
-                System.out.format("%c is victorious!", playerNum);
+                numberOfWins++;
+                return true;
             }
             else if (board[x][y] != '.'){
-                throw new Exception();
+                throw new Exception("Illegal move!");
             }
 
         } catch (Exception e) {
-            System.out.println("Illegal move, your turn is over");
             // Revert deletion of player
+            System.out.println("Illegal move, your turn is over");
             x = oldX;
             y = oldY;
         }
         finally{
-            System.out.format("x: %d, y: %d\n", x, y);
             board[x][y] = playerNum;
         }
+         return false;
     }
+
+    public void setLocation(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    boolean wantsToPlayAgain(Scanner stdin){
+        while (true) {
+            System.out.format("Player%c, would you like to keep playing?", playerNum);
+            String answer = stdin.nextLine();
+            if (!answer.equals("y") && !answer.equals("n")){
+                System.out.println("Please enter y or n (for yes or no)");
+                continue;
+            }
+            if (answer.equals("y")){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
 }
